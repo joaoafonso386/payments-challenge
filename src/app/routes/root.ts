4ca8d9c811c5a098ef7e65b3f9596dac8e1e1ccc
João Afonso <joaoafonso386@gmail.com>
2024-03-13 00:13:03 +0000

    Init fastify api

---
+++ src/app/routes/root.ts
@@ -0,0 +1,8 @@
+import { FastifyInstance } from 'fastify';
+
+export default async function (fastify: FastifyInstance) {
+  fastify.get('/', async function () {
+    return { message: 'Hello API' };
+  });
+  
+}
