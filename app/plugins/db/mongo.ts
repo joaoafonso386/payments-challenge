7323c9865986e598d9066e6beb6c56f171eb98d5
João Afonso <joaoafonso386@gmail.com>
2024-03-15 23:22:49 +0000

    Update mongo plugin

--- app/plugins/db/mongo.ts
+++ app/plugins/db/mongo.ts
@@ -3,9 +3,9 @@ import fp from 'fastify-plugin';
 import mongo from '@fastify/mongodb'
 
 /**
- * This plugins adds some utilities to handle http errors
+ * This plugins adds connections to a mongodb database
  *
- * @see https://github.com/fastify/fastify-sensible
+ * @see https://github.com/fastify/fastify-mongodb
  */
 export default fp(async function (fastify: FastifyInstance) {
   fastify.register(mongo, {
