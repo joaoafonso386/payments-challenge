834847092aa1b60243aa889d4eef8a5337d66d85
João Afonso <joaoafonso386@gmail.com>
2024-03-17 02:31:00 +0000

    Update initial mongo script to insert more users

--- docker/mongo-init.js
+++ docker/mongo-init.js
@@ -1,15 +1,25 @@
-db = db.getSiblingDB('payments')
+db = db.getSiblingDB('payments');
 
 db.createCollection('users');
 
-db.users.createIndex( { email: 1 }, { unique: true } )
-db.users.createIndex( { postCode: 1 }, { unique: true } )
+db.users.createIndex({ email: 1 }, { unique: true });
+db.users.createIndex({ postCode: 1 }, { unique: true });
 
-db.users.insert({
-  user: 'John',
-  surname: 'Doe',
-  postCode: '1800-057',
-  email: 'john@doe.com',
-  pwd: 'unhashed pwd',
-  type: 'test'
-});
+db.users.insert([
+  {
+    user: 'John',
+    surname: 'Doe',
+    postCode: '1800-057',
+    email: 'john@doe.com',
+    pwd: 'unhashed pwd',
+    type: 'user',
+  },
+  {
+    user: 'Mary',
+    surname: 'Doe',
+    postCode: '1800-051',
+    email: 'mary@doe.com',
+    pwd: 'unhashed pwd',
+    type: 'shopkeeper',
+  },
+]);
