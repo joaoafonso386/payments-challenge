5ecc4dae40aa80dca3252cf99f3bc724f80ad20e
João Afonso <joaoafonso386@gmail.com>
2024-03-18 00:18:16 +0000

    Add json schema validation for user

--- docker/mongo-init.js
+++ docker/mongo-init.js
@@ -3,15 +3,31 @@ db = db.getSiblingDB('payments');
 db.createCollection('users', {
   validator: {
     $jsonSchema: {
+      required: [ "name", "postCode", "email", "pwd", "type" ],
       bsonType: 'object',
       title: 'Users validation',
       properties: {
-        user: {},
-        surname: {},
-        postCode: {},
-        email: {},
-        pwd: {},
-        type: {}
+        user: {
+          bsonType: "string",
+          description: "must be a string and is required"
+        },
+        postCode: {
+          bsonType: "string",
+          description: "must be a string and is required"
+        },
+        email: {
+          bsonType: "string",
+          description: "must be a string and is required"
+        },
+        pwd: {
+          bsonType: "string",
+          description: "must be a string and is required"
+        },
+        type: {
+          bsonType: "string",
+          enum: [ "user", "shopkeeper" ],
+          description: "Must be either user, or shopkeeper"
+        }
       },
     },
   },
@@ -22,16 +38,14 @@ db.users.createIndex({ postCode: 1 }, { unique: true });
 
 db.users.insert([
   {
-    user: 'John',
-    surname: 'Doe',
+    user: 'John Doe',
     postCode: '1800-057',
     email: 'john@doe.com',
     pwd: 'unhashed pwd',
     type: 'user',
   },
   {
-    user: 'Mary',
-    surname: 'Doe',
+    user: 'Mary Doe',
     postCode: '1800-051',
     email: 'mary@doe.com',
     pwd: 'unhashed pwd',
