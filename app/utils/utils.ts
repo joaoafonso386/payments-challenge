938ac749b5da2da639a5f08919213598dac66b45
João Afonso <joaoafonso386@gmail.com>
2024-03-20 00:38:00 +0000

    Update validation for register logic and types

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,6 +1,20 @@
+import { User } from 'app/types/types';
 import { FastifyRequest } from "fastify"
 
-export const validateAuthBody = (req: FastifyRequest) => {
+export const validateAuthBody = (req: FastifyRequest<{ Body: User }>) => {
+
+    const { url, body } = req
+    const { name, pass } = body
+
+    if(url === "/register") {
+        if(!name || !pass)
+        return { isValid: true, error: 'no error' }
+    }
+    
+    if(url === "/login") {
+        return { isValid: true, error: 'no error' }
+    }
+    
     
     return { isValid: true, error: 'no error' }
 }
