6d9bdc411ab842bf9947d997bdf9021812d27b59
João Afonso <joaoafonso386@gmail.com>
2024-03-22 00:00:00 +0000

    Update /register with insert logic

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -7,7 +7,8 @@ export const validateAuthBody = (req: FastifyRequest<{ Body: User }>) => {
     const { name, pass, email, postCode } = body
 
     if(url === "/register") {
-        if (!name || !pass || !email || !postCode) return 'Must provide all arguments'
+        const errorRes = `Must provide name, password, email and post code, only provided ${JSON.stringify(body)}`
+        if (!name || !pass || !email || !postCode) return errorRes
         return false
     }
     
