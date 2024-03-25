41dbc30d496a2ace5df0a3b08a9336e0ccc75f23
João Afonso <joaoafonso386@gmail.com>
2024-03-25 17:30:09 +0000

    Add register logic (missing jwt)

--- app/routes/register.ts
+++ app/routes/register.ts
@@ -5,15 +5,27 @@ import { FastifyInstance } from 'fastify';
 export default async function register (f: FastifyInstance) {
   f.post<{ Body: User }>('/register', async (req, res) => {
 
+    let inserted = undefined
+    const { log }= req
     const error = validateAuthBody(req)
-
-    if(error) return { msg: 'You are not registered!', error }
-
-    const users =  f.mongo.db?.collection('users')
-    const pwd = hashPwd(req.body.pwd)
-    const inserted = await users?.insertOne(req.body)
-    return { msg: 'You are registered!' };
     
+    if(error) return res.code(403).send({ status: `${res.statusCode}`, msg: 'You are not registered!', error })
+   
+    log.info("Hashing password...")
+    const pwd = await hashPwd(req.body.pwd)
+    log.info("Inserting in DB...")
+
+    try {
+      const users =  f.mongo.db?.collection('users')
+      inserted = await users?.insertOne({ ...req.body, pwd })
+      log.info("User inserted!")
+    } catch(e) {
+      throw res.code(500).send({ status: `${res.statusCode}`, msg: `An error has occorred. ${e}` })
+    } 
+
+     //return jwt
+
+    return res.code(200).send({ status: `${res.statusCode}`, msg: "You are registered", inserted: inserted?.acknowledged })
   });
   
 }
