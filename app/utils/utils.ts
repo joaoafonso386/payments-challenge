e9c9812845e84d87862bcec44425e1bbd392f8ee
João Afonso <joaoafonso386@gmail.com>
2024-03-27 19:04:35 +0000

    Add email and post code validation

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,16 +1,18 @@
 import { hash } from '@node-rs/bcrypt';
 import { User } from 'app/types/types';
 import { FastifyRequest } from "fastify"
+import * as EmailValidator from 'email-validator';
+
 
 export const validateAuthBody = (req: FastifyRequest<{ Body: User }>) => {
 
     const { url, body } = req
     const { name, pwd, email, postCode, type } = body
 
-    //check if email and postCode are valid
+    //TODO: check if email is valid
     if(url === "/register") {
-        const errorRes = `Must provide name, password, email, post code and type. Only provided ${JSON.stringify(body)}`
-        if (!name || !pwd || !email || !postCode || !type) return errorRes
+        const errorRes = `Must provide name, password, email, post code and type. Must have a valid postCode and email. Provided ${JSON.stringify(body)}`
+        if (!name || !pwd || !email || !postCode || !type || !validatePostCode(postCode) || !validateEmail(email)) return errorRes
         return false
     }
     
@@ -27,3 +29,10 @@ export const hashPwd = async (pwd: string) => {
     return await hash(pwd)
 }
 
+export const validatePostCode = (postCode: string) => {
+    return /^[1-9][0-9]{3}-[0-9]{3}$/.test(postCode) 
+}
+
+export const validateEmail = (email: string) => {
+    return EmailValidator.validate(email)
+}
