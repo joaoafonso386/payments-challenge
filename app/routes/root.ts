53fb9ed0a3830c46c5b84b9cf404d9ea0d6af9de
João Afonso <joaoafonso386@gmail.com>
2024-04-04 00:39:49 +0100

    Cleanup for initial routes

--- app/routes/root.ts
+++ app/routes/root.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance } from 'fastify';
 
 export default async function (f: FastifyInstance) {
-  f.get('*', async function () {
-    return { message: 'This route does not exist' };
-  });
+  f.get('*', async () => ({
+    message: 'This route does not exist' 
+  }))
 
-  f.get('/', async function () {
-    return { message: 'Welcome to the payments-challenge API' };
-  });
+  f.get('/', async () => ({
+    message: 'Welcome to the payments-challenge API!' 
+  }))
   
 }
