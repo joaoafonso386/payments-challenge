8ab6c9d740bc57ea9aad01534af8c1bac155f740
João Afonso <joaoafonso386@gmail.com>
2024-04-15 19:38:36 +0100

    Update middleware docs

--- app/plugins/middleware/middleware.ts
+++ app/plugins/middleware/middleware.ts
@@ -4,13 +4,13 @@ import { FastifyInstance } from "fastify";
 import { auth } from './auth';
 
 /**
- * This plugins adds middleware capabilities
+ * This plugins adds middleware capabilities (more than just fastify hooks)
  *
  * @see https://github.com/fastify/middie
  */
 
 export default fp(async (f: FastifyInstance, opts) => {
     await f.register(middie);
-    f.use('/transfer', auth); // Apply to route only to route /transfer. Flexible for pattern/dir like routes
+    f.use('/transfer', auth); // Apply only to route /transfer. Flexible for pattern/dir like routes
 })
   
