863f7b84d28b0b3eb7bae8f344ecc8d2ff42f77e
João Afonso <joaoafonso386@gmail.com>
2024-04-19 00:46:48 +0100

    Add transfer validation for user existance, funds and invalid token

--- app/routes/transfer.ts
+++ app/routes/transfer.ts
@@ -1,33 +1,42 @@
 import { TokenPayload, User, UserType } from "../types/types";
 import { FastifyInstance } from "fastify";
-import { verify } from "jsonwebtoken";
+import { JsonWebTokenError, verify } from "jsonwebtoken";
 
 //logic for a payment (transfer)
 export default async function transfer (f: FastifyInstance) {
-    f.post<{ Body: { receiver: string, amout: string } }>('/transfer', async (req, res) => {
+    f.post<{ Body: { receiver: string, amount: string } }>('/transfer', async (req, res) => {
 
       if(!req.headers.authorization) throw new Error("Authorization header is missing")
       
       const tokenParts = req.headers.authorization?.split(" ")
       const receiver = req.body.receiver
-      const amout = parseInt(req.body.amout)
+      const amount = parseInt(req.body.amount)
 
       if(tokenParts.length !== 2 || tokenParts[0] !== "Bearer") throw new Error("Invalid authorization header format")
       
       const token = tokenParts[1]
 
       try {
-        const userToken = verify(token, `${process.env.SECRET_KEY}`) as TokenPayload
-        if(userToken.type !== UserType.USER) {
+        const senderToken = verify(token, `${process.env.SECRET_KEY}`) as TokenPayload
+        if(senderToken.type !== UserType.USER) {
           throw new Error("You are not a user, transfers are not available")
         }
         const usersCollection =  f.mongo.db?.collection('users')
-        const users = await usersCollection?.find<User>({ email: { $in: [userToken.email, receiver] }}).toArray()
-        // if(user?.balance|| 0 < amout) {
-        //   throw new Error("Insufficient founds for transaction")
-        // }
-      } catch {
-        throw new Error("Invalid Token")
+        const users = await usersCollection?.find<User>({ email: { $in: [senderToken.email, receiver] }}).toArray() || []
+        if(users?.length < 2) {
+          throw new Error("Receiver or sender does not exist")
+        }
+        const sender = users.find(user => user.email === senderToken.email)
+        const balance = sender?.balance || 0
+        if(amount > balance) {
+          throw new Error("Insufficient funds for transfer")
+        }
+
+      } catch (e: unknown) {
+        if(e instanceof JsonWebTokenError){
+          throw new Error("Invalid token")
+        }
+        throw new Error(`${e}`)
       }
 
 
