c5ab9d8ff86a1ab1b2372b1a931d2a4fa209f560
João Afonso <joaoafonso386@gmail.com>
2024-04-26 02:44:03 +0100

    Add logic to update balance of sender and receiver when transfer is made

--- app/routes/transfer.ts
+++ app/routes/transfer.ts
@@ -1,37 +1,48 @@
-import { TokenPayload, User, UserType } from "../types/types";
-import { FastifyInstance } from "fastify";
-import { JsonWebTokenError, verify } from "jsonwebtoken";
+import { TokenPayload, User, UserType } from '../types/types';
+import { FastifyInstance } from 'fastify';
+import { JsonWebTokenError, verify } from 'jsonwebtoken';
 
-export default async function transfer (f: FastifyInstance) {
-    f.post<{ Body: { receiver: string, amount: string } }>('/transfer', async (req, res) => {
+export default async function transfer(f: FastifyInstance) {
+  f.post<{ Body: { receiver: string; amount: string } }>(
+    '/transfer',
+    async (req, res) => {
+      if (!req.headers.authorization)
+        throw new Error('Authorization header is missing');
 
-      if(!req.headers.authorization) throw new Error("Authorization header is missing")
-      
-      const tokenParts = req.headers.authorization?.split(" ")
-      const receiverEmail = req.body.receiver
-      const amount = parseInt(req.body.amount)
+      const tokenParts = req.headers.authorization?.split(' ');
+      const receiverEmail = req.body.receiver;
+      const amount = parseInt(req.body.amount);
 
-      if(tokenParts.length !== 2 || tokenParts[0] !== "Bearer") throw new Error("Invalid authorization header format")
-      
-      const token = tokenParts[1]
+      if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer')
+        throw new Error('Invalid authorization header format');
+
+      const token = tokenParts[1];
 
       try {
-        const senderToken = verify(token, `${process.env.SECRET_KEY}`) as TokenPayload
-        if(senderToken.type !== UserType.USER) {
-          throw new Error("You are not a user, transfers are not available")
+        const senderToken = verify(
+          token,
+          `${process.env.SECRET_KEY}`
+        ) as TokenPayload;
+        if (senderToken.type !== UserType.USER) {
+          throw new Error('You are not a user, transfers are not available');
         }
-        if(senderToken.email === receiverEmail) {
-          throw new Error("You cannot transfer to yourself")
+        if (senderToken.email === receiverEmail) {
+          throw new Error('You cannot transfer to yourself');
         }
-        const usersCollection =  f.mongo.db?.collection('users')
-        const users = await usersCollection?.find<User>({ email: { $in: [senderToken.email, receiverEmail] }}).toArray() || []
-        if(users?.length < 2) {
-          throw new Error("Receiver or sender does not exist")
+        const usersCollection = f.mongo.db?.collection('users');
+        const users =
+          (await usersCollection
+            ?.find<User>({ email: { $in: [senderToken.email, receiverEmail] } })
+            .toArray()) || [];
+        if (users?.length < 2) {
+          throw new Error('Receiver or sender does not exist');
         }
-        const [sender, receiver] = users.sort((a, b) => a.email === senderToken.email ? -1 : 0) 
-        const balance = sender?.balance || 0
-        if(amount > balance) {
-          throw new Error("Insufficient funds for transfer")
+        const [sender, receiver] = users.sort((a) =>
+          a.email === senderToken.email ? -1 : 0
+        );
+        const balance = sender?.balance ?? 0;
+        if (amount > balance) {
+          throw new Error('Insufficient funds for transfer');
         }
 
         const service = await fetch("https://util.devi.tools/api/v2/authorize")
@@ -40,19 +51,40 @@ export default async function transfer (f: FastifyInstance) {
           throw new Error("External confirmation not guaranteed")
         }
 
-        const transfersCollection =  f.mongo.db?.collection('transfers')
-        const transfer = await transfersCollection?.insertOne({ sender: senderToken.email, receiver, amount, date: new Date(Date.now()).toISOString() })
+        const transfersCollection = f.mongo.db?.collection('transfers');
 
+        await transfersCollection?.insertOne({
+          sender: senderToken.email,
+          receiver: receiver.email,
+          amount,
+          date: new Date(Date.now()).toISOString(),
+        });
+        req.log.info("Transfer registered")
+        await usersCollection?.updateMany(
+          { $or: [{ email: senderToken.email }, { email: receiver.email }] },
+          [
+            {
+              $set: {
+                balance: {
+                  $cond: {
+                    if: { $eq: ['$email', senderToken.email] },
+                    then: { $subtract: ["$balance", amount] },
+                    else: { $add: ["$balance", amount] },
+                  },
+                },
+              },
+            }
+          ]
+        );
+        req.log.info("Balance updated for sender and receiver")
       } catch (e: unknown) {
-        if(e instanceof JsonWebTokenError){
-          throw new Error("Invalid token")
+        if (e instanceof JsonWebTokenError) {
+          throw new Error('Invalid token');
         }
-        throw new Error(`${e}`)
+        throw new Error(`${e}`);
       }
 
-     
-
-      return res.code(200).send({ msg: "Transfer made" })
-    });
-    
+      return res.code(200).send({ msg: 'Transfer made' });
+    }
+  );
 }
